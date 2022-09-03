8d55e40a7701c4813c794f73f1850db716e7210e
UlfetZakirli <ulfetzakirli@gmail.com>
2022-09-03 19:27:35 +0400

    sixth task was started

--- day5-blurry loading/script.js
+++ day5-blurry loading/script.js
@@ -1,22 +1,20 @@
-const loadingText = document.querySelector('.loading-text')
+const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
-
 let load = 0
+let int = setInterval(blurry, 30)
 
-let int = setInterval(blurring, 30)
 
-function blurring() {
+function blurry() {
     load++
     if (load > 99) {
         clearInterval(int)
     }
-    loadingText.innerText = `${load}%`
-    loadingText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0,100,30,0)}px)`
+    loadText.innerHTML = `${load}%`
+    loadText.style.opacity=scale(load,0,100,1,0)
+    bg.style.filter=`blur(${scale(load,0,100,30,0)}px)`
 }
 
 
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-  }
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
