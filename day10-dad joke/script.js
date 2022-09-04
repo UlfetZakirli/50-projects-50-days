c540e0fb94b06780e73e1f2bcd8f340216def440
UlfetZakirli <ulfetzakirli@gmail.com>
2022-09-04 14:20:12 +0400

    tenth task completed

---
+++ day10-dad joke/script.js
@@ -0,0 +1,31 @@
+const jokeElement = document.getElementById('joke')
+const jokeButton = document.getElementById('jokeBtn')
+
+generateJoke()
+jokeButton.addEventListener('click',generateJoke)
+
+
+//using Async/Await
+async function generateJoke(){
+
+    const config={
+        headers:{
+            'Accept':'application/json'
+        }
+    }
+   const res=await fetch('https://icanhazdadjoke.com',config)
+   const data= await res.json('')
+   jokeElement.innerHTML=data.joke
+}
+
+//using .then()
+// function generateJoke() {
+//     const config={
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then((res) => res.json(''))
+//         .then((data) =>jokeElement.innerHTML=data.joke )
+// }
