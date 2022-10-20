36412c888874db61ee18e2f34b3b82d48395eccc
UlfetZakirli <ulfetzakirli@gmail.com>
2022-10-20 17:41:52 +0400

    javaScript: project is completed

--- day27-toast notification/script.js
+++ day27-toast notification/script.js
@@ -1,35 +1,36 @@
-const toasts=document.getElementById('toasts')
-const button=document.getElementById('button')
+const toasts = document.getElementById('toasts')
+const button = document.getElementById('button')
 
-const messages=[
+const messages = [
     'Message One',
     'Message Two',
     'Message Three',
     'Message Four'
 ]
-const types=[
-    'success','info','error'
+const types = [
+    'success', 'info', 'error'
 ]
 
-button.addEventListener('click',()=>createNotification())
+button.addEventListener('click', () => createNotification())
 
-const createNotification=(message=null, type=null)=>{
-const notif=document.createElement('div')
-notif.classList.add('toast')
-notif.classList.add(type? type:getRandomType())
+const createNotification = (message = null, type = null) => {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+    notif.classList.add(type ? type : getRandomType())
 
-notif.innerText=message? message : getRandomMessage()
-toasts.appendChild(notif)
+    notif.innerText = message ? message : getRandomMessage()
+    toasts.appendChild(notif)
 
 
-setInterval(()=>{
-    notif.remove()
-},3000)
-}
-function getRandomType(){
-    return types[Math.floor(Math.random()*types.length)]
+    setInterval(() => {
+        notif.remove()
+    }, 3000)
 }
 
-const getRandomMessage=()=>{
-    return messages[Math.floor(Math.random()*messages.length)]
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)]
+}
+
+const getRandomMessage = () => {
+    return messages[Math.floor(Math.random() * messages.length)]
 }
